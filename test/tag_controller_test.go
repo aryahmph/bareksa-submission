a5996711649652dabeaf963bb6d31b402fbd1320
Arya Yunanta <aryayun90@gmail.com>
2021-09-17 16:18:20 +0700

    Add Tag List API test

--- test/tag_controller_test.go
+++ test/tag_controller_test.go
@@ -5,8 +5,10 @@ import (
 	"bareksa-aryayunanta/controller"
 	"bareksa-aryayunanta/helper"
 	"bareksa-aryayunanta/middleware"
+	"bareksa-aryayunanta/model/domain"
 	"bareksa-aryayunanta/repository"
 	"bareksa-aryayunanta/service"
+	"context"
 	"database/sql"
 	"encoding/json"
 	"github.com/go-playground/validator/v10"
@@ -95,4 +97,44 @@ func TestCreateTagValidationFailed(t *testing.T) {
 
 	assert.Equal(t, 400, int(responseBody["code"].(float64)))
 	assert.Equal(t, "BAD REQUEST", responseBody["status"])
+}
+
+func TestListTagSuccess(t *testing.T) {
+	db := setupTestDB()
+	truncateTags(db)
+
+	tx, _ := db.Begin()
+	tagRepository := repository.NewTagRepositoryImpl()
+	tag1 := tagRepository.Save(context.Background(), tx, domain.Tag{Name: "investment"})
+	tag2 := tagRepository.Save(context.Background(), tx, domain.Tag{Name: "internship"})
+	tx.Commit()
+
+	router := setupRouter(db)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/api/tags", nil)
+	request.Header.Add("X-API-Key", "BAREKSA_INTERNSHIP")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
+
+	tags := responseBody["data"].([]interface{})
+
+	tagResponse1 := tags[1].(map[string]interface{})
+	tagResponse2 := tags[0].(map[string]interface{})
+
+	assert.Equal(t, tag1.ID, uint32(tagResponse1["id"].(float64)))
+	assert.Equal(t, tag1.Name, tagResponse1["name"])
+
+	assert.Equal(t, tag2.ID, uint32(tagResponse2["id"].(float64)))
+	assert.Equal(t, tag2.Name, tagResponse2["name"])
 }
