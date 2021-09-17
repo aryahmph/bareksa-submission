b2054efa136c86972425b5131facb02a22640afb
Arya Yunanta <aryayun90@gmail.com>
2021-09-17 08:08:03 +0700

    Add Helper func

--- helper/model.go
+++ helper/model.go
@@ -1,1 +1,21 @@
 package helper
+
+import (
+	"bareksa-aryayunanta/model/domain"
+	"bareksa-aryayunanta/model/web"
+)
+
+func ToTagResponse(tag domain.Tag) web.TagResponse {
+	return web.TagResponse{
+		Id:   tag.ID,
+		Name: tag.Name,
+	}
+}
+
+func ToTagResponses(tags []domain.Tag) []web.TagResponse {
+	var tagResponses []web.TagResponse
+	for _, tag := range tags {
+		tagResponses = append(tagResponses, ToTagResponse(tag))
+	}
+	return tagResponses
+}
