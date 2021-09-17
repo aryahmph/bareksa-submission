f0e00d6c6beda7e4d4dac830faececd8dc716dbc
Arya Yunanta <aryayun90@gmail.com>
2021-09-17 09:31:46 +0700

    Add AlreadyExist exception

---
+++ exception/error_handler.go
@@ -0,0 +1,95 @@
+package exception
+
+import (
+	"bareksa-aryayunanta/helper"
+	"bareksa-aryayunanta/model/web"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+)
+
+func ErrorHandler(writer http.ResponseWriter, request *http.Request, err interface{}) {
+
+	if notFoundError(writer, request, err) {
+		return
+	}
+
+	if alreadyExistError(writer, request, err) {
+		return
+	}
+
+	if validationErrors(writer, request, err) {
+		return
+	}
+
+	internalServerError(writer, request, err)
+}
+
+func validationErrors(writer http.ResponseWriter, request *http.Request, err interface{}) bool {
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "BAD REQUEST",
+			Data:   exception.Error(),
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+		return true
+	} else {
+		return false
+	}
+}
+
+func notFoundError(writer http.ResponseWriter, request *http.Request, err interface{}) bool {
+	exception, ok := err.(NotFoundError)
+	if ok {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusNotFound)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusNotFound,
+			Status: "NOT FOUND",
+			Data:   exception.Error,
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+		return true
+	} else {
+		return false
+	}
+}
+
+func alreadyExistError(writer http.ResponseWriter, request *http.Request, err interface{}) bool {
+	exception, ok := err.(AlreadyExistError)
+	if ok {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusConflict)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusConflict,
+			Status: http.StatusText(http.StatusConflict),
+			Data:   exception.Error,
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+		return true
+	} else {
+		return false
+	}
+}
+
+func internalServerError(writer http.ResponseWriter, request *http.Request, err interface{}) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusInternalServerError)
+
+	webResponse := web.WebResponse{
+		Code:   http.StatusInternalServerError,
+		Status: "INTERNAL SERVER ERROR",
+		Data:   err,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
